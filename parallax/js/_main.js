a7187270f03c01ec56a6f96dbc138e500e14d3d5
Elizabeth Li <etli2013@gmail.com>
2016-08-11 15:27:27 -0700

    use css to adjust slide height instead of js

--- parallax/js/_main.js
+++ parallax/js/_main.js
@@ -12,20 +12,17 @@
   
   // Setup variables
   $window = $(window);
-  $slide = $('.homeSlide');
-  $slideTall = $('.homeSlideTall');
-  $slideTall2 = $('.homeSlideTall2');
   $body = $('body');
   
     //FadeIn all sections   
   $body.imagesLoaded( function() {
     setTimeout(function() {
-          
-          // Resize sections
-          adjustWindow();
-          
-          // Fade in sections
-        $body.removeClass('loading').addClass('loaded');
+      
+      // Resize sections
+      adjustWindow();
+      
+        // Fade in sections
+      $body.removeClass('loading').addClass('loaded');
         
     }, 800);
   });
@@ -41,24 +38,10 @@
         }
     });
 
-    skrollr.menu.init(s);
-    
-    // Get window size
-      winH = $window.height();
-      
-      // Keep minimum height 550
-      if(winH <= 550) {
-      winH = 550;
-    } 
-      
-      // Resize our slides
-      $slide.height(winH);
-      $slideTall.height(winH*2);
-      $slideTall2.height(winH*3);
-      
-      // Refresh Skrollr after resizing our sections
-      s.refresh($('.homeSlide'));
-      
+    // Init Skrollr menu
+    skrollr.menu.init(s, {
+      updateUrl: false
+    });
   }
     
 } )( jQuery );
